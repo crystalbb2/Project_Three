a2d2e367d4506dd3d4b87a963b149a528761f646
crystalbb2 <crystaldrago2@gmail.com>
2024-10-31 19:07:05 +0000

    First commit of new function to R package

--- R/new_function2.R
+++ R/new_function2.R
@@ -1,7 +1,15 @@
+#' Multiplies hindfoot length and weight
+#'
+#' @param dataframe The path to the tab-delimited file you want to process
+#' @param hindfoot_length The length of wood rats' hindfoot in mm
+#' @param weight The weight of wood rats in grams
+#'
+#' @return Error message for numbers greater than 5500
+#' @export
 new_function2 = function(dataframe, hindfoot_length, weight){
-  dataframe = read_csv("data/surveys.csv") %>% 
-    select(hindfoot_length, weight) %>% 
-    filter(!is.na(hindfoot_length)) %>% 
+  dataframe = read_csv("data/surveys.csv") %>%
+    select(hindfoot_length, weight) %>%
+    filter(!is.na(hindfoot_length)) %>%
     filter(!is.na(weight))
   result = dataframe$hindfoot_length*dataframe$weight
   result = na.omit(result)
